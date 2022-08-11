cac68c2bb18359d3fa357072fac151dd6de8799c
Gustavo <gustavodbeltran@gmail.com>
2022-08-11 09:44:11 -0300

    agregué login y logout

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/servicios/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  isUserLogged: Boolean = false;
+
+  constructor(private authservice:AuthService,
+              private router:Router) { }
 
   ngOnInit(): void {
+    this.isUserLogged = this.authservice.isUserLogged();
+    this.router.navigate(['/home']);
+  }
+  logout(): void {
+    this.authservice.logout();
+    this.isUserLogged = false;
+    window.location.reload();
   }
 
 }
---
+++ src/data/UsuarioNuevo.ts
@@ -0,0 +1,6 @@
+export interface UsuarioNuevo {
+    id: number;
+    nombre: string;
+    email: string;
+    password: string;
+}
