49ca6293c367706ed56f7a6042d0fa5abdb4de66
Gustavo <gustavodbeltran@gmail.com>
2022-08-17 10:22:27 -0300

    trabajando en validaciones del formulario comanterios

--- src/app/components/comentarios/comentarios.component.ts
+++ src/app/components/comentarios/comentarios.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ComentariosService } from 'src/app/servicios/comentarios.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-comentarios',
@@ -7,9 +10,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComentariosComponent implements OnInit {
 
-  constructor() { }
+  formComentarios: FormGroup;
+  isAlert = false;
+
+  constructor( private formBuilder: FormBuilder,
+               private comentariosSVC: ComentariosService,
+               private router: Router) {
+    this.formComentarios = this.formBuilder.group(
+      {
+        id_comentario: ['', [Validators.required]],
+        id_usuario: ['', [Validators.required]],
+        nombre_usuario: ['', [Validators.required]],
+        foto_perfil: [''],
+        texto: ['', [Validators.required, Validators.maxLength(200)]],
+        fecha_publicado: ['date']
+      }
+    )
+   }
 
   ngOnInit(): void {
+   
+  }
+  public publicarComentario(event: Event){
+    
+    this.clearFormulario();
+    this.isAlert=true;
+    console.log("formulario de comentarios" + this.formComentarios.value);
+   
+  }
+
+  clearFormulario() {
+    this.formComentarios.setValue({
+      id_comentario: '',
+      id_usuario: '',
+      nombre_usuario: '',
+      foto_perfil: '',
+      texto: '',
+      fecha_publicado: ''
+    })
+  }
+  get Texto() {
+    return this.formComentarios.get('texto');
   }
 
 }
+ 
---
+++ src/data/IComentarios.ts
@@ -0,0 +1,8 @@
+export interface IComentarios {
+    id_comentario: number;
+    id_usuario: number;
+    nombre_usuario: String;
+    foto_perfil: String;
+    texto:String;
+    fecha_publicado: String;
+}
