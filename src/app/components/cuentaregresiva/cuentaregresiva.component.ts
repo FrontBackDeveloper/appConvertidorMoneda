6354f68f74c6e45352bdeae916777581e12d9ddd
Gustavo <gustavodbeltran@gmail.com>
2022-08-23 09:43:36 -0300

    cuentaregrasiva lista, paso a compnente presupuesto

--- src/app/components/cuentaregresiva/cuentaregresiva.component.ts
+++ src/app/components/cuentaregresiva/cuentaregresiva.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { interval } from 'rxjs/internal/observable/interval';
+import { map } from 'rxjs/internal/operators/map';
 @Component({
   selector: 'app-cuentaregresiva',
   templateUrl: './cuentaregresiva.component.html',
@@ -7,9 +8,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CuentaregresivaComponent implements OnInit {
 
-  constructor() { }
+  time!: {
+    days: number;
+    hours: number;
+    minutes: number;
+    seconds: number;
+  };
+  @Input() finishDateString: string = '';
+  finishDate: Date = new Date();
+  
 
   ngOnInit(): void {
+    // Inicializamos el momento que falta hasta llegaral tiempo objetivo con valores en 0
+    this.time = {
+      days: 0, hours: 0, minutes: 0, seconds: 0
+    };
+    // Creamos la fecha a partir de la fecha en formato string AAAA-MM-dd HH:mm:ss
+    this.finishDate = new Date(this.finishDateString); 
+
+    let counterTimer$ = this.start().subscribe((_) => {
+      if (this.time.days <= 0) {
+          this.time = {
+            hours: 0,
+            minutes: 0,
+            seconds: 0,
+            days: 0
+          }
+          counterTimer$.unsubscribe();
+      }
+  });
+  }
+
+  updateTime() {
+    
+    const now = new Date();
+    const diff = this.finishDate.getTime() - now.getTime();
+   
+
+    // Cálculos para sacar lo que resta hasta ese tiempo objetivo / final
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(diff / (1000 * 60 * 60));
+    const mins = Math.floor(diff / (1000 * 60));
+    const secs = Math.floor(diff / 1000);
+    
+    // La diferencia que se asignará para mostrarlo en la pantalla
+    this.time.days = days;
+    this.time.hours = hours - days * 24;
+    this.time.minutes = mins - hours * 60;
+    this.time.seconds = secs - mins * 60;
+  }
+  
+  // Ejecutamos la acción cada segundo, para obtener la diferencia entre el momento actual y el objetivo
+  start() {
+    return interval(1000).pipe(
+      map((x: number) => {
+        this.updateTime();
+        return x;
+      })
+    );
   }
 
 }
---
+++ src/data/IPresupuesto.ts
@@ -0,0 +1,6 @@
+export interface IPresupuesto {
+    id_presupuesto: number;
+    id_usuario: number;
+    nombre_presupuesto: String;
+    fecha_publicado: String;
+}
